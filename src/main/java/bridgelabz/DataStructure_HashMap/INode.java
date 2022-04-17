62af63ac20ffed47f38cd31f7d58d7102009b0ac
sameers999 <syedmahammadsameer999>
2022-04-17 20:13:26 +0530

    Added DataStructure Programs

---
+++ src/main/java/bridgelabz/DataStructure_HashMap/INode.java
@@ -0,0 +1,14 @@
+package bridgelabz.DataStructure_HashMap;
+
+public interface INode<K> {
+	
+		public void setKey(K key);
+
+		public K getKey();
+
+		public void setNext(INode<K> next);
+
+		public INode<K>	getNext();
+	
+
+}
